1833996a4f984852f8bcdaddc45f070506f1ff67
phuc_le_ngoc <lengocphuc0099@gmail.com>
2023-05-28 16:17:35 +0700

    add profile page

--- src/components/posts/Posts.tsx
+++ src/components/posts/Posts.tsx
@@ -19,7 +19,7 @@ const Posts = () => {
       name: "John Doe",
       userId: "1",
       profilePic:
-        "https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?auto=compress&cs=tinysrgb&w=1600",
+        "https://images.pexels.com/photos/14028501/pexels-photo-14028501.jpeg?auto=compress&cs=tinysrgb&w=1600",
       desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
       img: "https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600",
     },
